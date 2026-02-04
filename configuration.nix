ee5bb9f412ed22902462ffa8ba32826c64d9ba2c
Jeremy Gao <wensimehrp@gmail.com>
2026-02-04 09:42:48 -0800

    feat: enable bluetooth

--- configuration.nix
+++ configuration.nix
@@ -212,6 +212,9 @@
     amberol
   ];
 
+  # bluetooth stuff
+  hardware.bluetooth.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
