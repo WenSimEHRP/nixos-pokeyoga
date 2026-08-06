01da3b67178e950206b80e9ddf2c262442daac8c
Jeremy Gao <wensimehrp@gmail.com>
2026-08-06 05:12:55 -0700

    feat: add libreoffice

--- configuration.nix
+++ configuration.nix
@@ -174,6 +174,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    # school
+    libreoffice
     # theming related
     adw-gtk3
     qadwaitadecorations
